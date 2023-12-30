b067ec0a52e28e6825e7bd2d9972c63c7bce4061
Mat Gargano <mat@statenweb.com>
2023-12-30 15:08:43 -0500

    k

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Replace environment variables in the Nginx non-SSL configuration template
-envsubst '${DOMAIN_NAME} ${PORT}' < /etc/nginx/nginx.conf.tpl > /etc/nginx/nginx.conf
+envsubst '$DOMAIN_NAME' < /etc/nginx/nginx.conf.tpl > /etc/nginx/nginx.conf
+
+# Remove worker_processes directive from default.conf
+sed -i '/worker_processes/d' /etc/nginx/conf.d/default.conf
 
 # Start Nginx with the initial non-SSL configuration
 nginx &
@@ -16,7 +19,7 @@ certbot --nginx -d $DOMAIN_NAME -m $EMAIL --agree-tos --no-eff-email --keep-unti
 nginx -s stop
 
 # Replace environment variables in the Nginx SSL configuration template
-envsubst '${DOMAIN_NAME} ${PORT}' < /etc/nginx/nginx.conf.ssl.tpl > /etc/nginx/conf.d/default.conf
+envsubst '$DOMAIN_NAME' < /etc/nginx/nginx.conf.ssl.tpl > /etc/nginx/nginx.conf
 
 # Restart Nginx with SSL configuration
 nginx -g 'daemon off;'
